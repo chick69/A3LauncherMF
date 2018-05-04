dce75cf5c08fbf29d065219d8bc794f2efef893b
SANTUCCI Lionel <lionel.santucci69@numericable.fr>
2018-05-04 15:31:14 +0200

    depart gestion addons serveur

---
+++ src/UMajAddonServer.pas
@@ -0,0 +1,25 @@
+unit UMajAddonServer;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
